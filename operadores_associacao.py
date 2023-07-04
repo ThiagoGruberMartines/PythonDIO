20fec65156204578c878541e536491527e6235bc
Thiago Gruber Martines <111470513+ThiagoGruberMartines@users.noreply.github.com>
2023-07-04 17:34:21 -0400

    Update operadores_associacao.py

---
+++ operadores_associacao.py
@@ -0,0 +1,9 @@
+nome = "Thiago Gruber Martines"
+compras = ["Carne", "Arroz", "Feijão"]
+saques = [500, 200]
+
+print("Martines" in nome)
+print("iag" in nome)
+print("tines" in nome)
+print("Batata" not in compras)
+print(100 in saques)
