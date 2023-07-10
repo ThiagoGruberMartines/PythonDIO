9aac5001282f08312fd3a451ef5c8535a84ec393
Thiago Gruber Martines <111470513+ThiagoGruberMartines@users.noreply.github.com>
2023-07-10 07:36:06 -0400

    .

---
+++ estrutura_repeticao_break.py
@@ -0,0 +1,14 @@
+# while True: 
+#    numero = int(input("Informe um número: "))
+
+#    if numero == 10:
+#        break
+
+# print(numero)
+
+for numero in range(11):
+
+    if numero == 5:
+        continue # break para encerrar definitivamente, continue para continuar sem considerar o valor informado
+
+    print(numero, end=" ")
---
+++ estrutura_repeticao_for.py
@@ -0,0 +1,16 @@
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+# exemplo utilizando um iterável
+for letra in texto:
+    if letra.upper() in VOGAIS: 
+        print(letra, end="")
+else:
+    print() # adiciona uma quebra de linha
+    print("Executa no final do laço")
+
+
+
+ # exemplo com a função built-in range
+for numero in range(0, 51, 5):
+    print(numero)
